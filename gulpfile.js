2ecac83825992e4bd80f27834dd0b68db0079299
petrero <petrero@gmail.com>
2017-05-20 11:56:06 +0300

    Configuration Variables
    But we are starting to have some duplication - I've got the app/Resources/assets/... path in 2 places. This is just normal JavaScript, so let's create a variable called config. Hey! I'll even add the equals sign. Let's store some paths on here, like assetsDir set to app/Resources/assets and sassPattern set to sass/**/*.scss.
    
    Now we can just use these variables - classic programming! So,config.assetsDir, a / in the middle, then config.sassPattern. Put that in both places.
    
    To stop the watch task, type ctrl+c. I'll run gulp sass to make sure I didn't break anything. It's happy with the config!

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+var config = {
+  assetsDir: 'app/Resources/assets',
+  sassPattern: 'sass/**/*.scss'
+}
+
 gulp.task('sass', function(){
-   gulp.src('app/Resources/assets/sass/**/*.scss')
+   gulp.src(config.assetsDir + '/' + config.sassPattern)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('.'))
@@ -11,7 +16,7 @@ gulp.task('sass', function(){
 });
 
 gulp.task('watch', function(){
-  gulp.watch('app/Resources/assets/sass/**/*.scss', ['sass'])
+  gulp.watch(config.assetsDir + '/' + config.sassPattern, ['sass'])
 });
 
 gulp.task('default', ['sass']);
