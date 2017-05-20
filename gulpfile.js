75dca886898c0e5cbd2aa6e9c9dfe6953527c3bb
petrero <petrero@gmail.com>
2017-05-20 11:04:03 +0300

    Copy code from branch 2

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function(){
+  gulp.src('app/Resources/assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('web/css'));
+});
