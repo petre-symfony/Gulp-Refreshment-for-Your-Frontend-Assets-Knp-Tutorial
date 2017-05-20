a59c525cd947cfd813e5e0df935fc08d22d40076
petrero <petrero@gmail.com>
2017-05-20 12:30:26 +0300

    Next, go steal the require line and paste it on top.
    Oh, and let's put a var before that.
    
    And once again, we're going to use the trusty pipe() function to push things through minifyCSS()
    
    This is looking really nice. Oh, and most of these functions - like minifyCSS() or sass() do take some options. So if you need to customize how things are minified, you can totally do that.
    
    Ok, go back and run gulp!
    
    gulp
    And now main.css is a single line. BUT, through the power of sourcemaps, we still get the correct styles.scss line in the inspector.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
 
 var config = {
   assetsDir: 'app/Resources/assets',
@@ -13,6 +14,7 @@ gulp.task('sass', function(){
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(concat('main.css'))
+    .pipe(minifyCSS())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('web/css')); 
 });
