57188e93ce911d565a1beab2ac51d3f38be8659e
petrero <petrero@gmail.com>
2017-05-20 13:02:20 +0300

    Copy code from branch 8

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var util = require('gulp-util');
+var gulpif = require('gulp-if');
+
+
+var config = {
+  assetsDir: 'app/Resources/assets',
+  sassPattern: 'sass/**/*.scss',
+  production: !!util.env.production,
+  sourceMaps: !util.env.production
+}
+
+gulp.task('sass', function(){
+   gulp.src(config.assetsDir + '/' + config.sassPattern)
+    .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(config.production ? minifyCSS() : util.noop())
+    .pipe(gulpif(config.sourceMaps, sourcemaps.write('.')))
+    .pipe(gulp.dest('web/css')); 
+});
+
+gulp.task('watch', function(){
+  gulp.watch(config.assetsDir + '/' + config.sassPattern, ['sass'])
+});
+
+gulp.task('default', ['sass', 'watch']);
