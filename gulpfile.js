78493a11c27c00c65cd10aea560ec9ac20cae541
petrero <petrero@gmail.com>
2017-05-20 12:33:13 +0300

    Copy code from branch 6

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+
+var config = {
+  assetsDir: 'app/Resources/assets',
+  sassPattern: 'sass/**/*.scss'
+}
+
+gulp.task('sass', function(){
+   gulp.src(config.assetsDir + '/' + config.sassPattern)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('web/css')); 
+});
+
+gulp.task('watch', function(){
+  gulp.watch(config.assetsDir + '/' + config.sassPattern, ['sass'])
+});
+
+gulp.task('default', ['sass', 'watch']);
